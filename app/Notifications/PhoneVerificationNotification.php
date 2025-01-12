4272f313e045b5adaf6c1f6af4839152b014d051
RhymeRone <mavi-okyanus63@hotmail.com>
2025-01-12 19:43:46 +0300

    Onay kodlari yaninda onay sayfasi linki de eklendi

--- app/Notifications/PhoneVerificationNotification.php
+++ app/Notifications/PhoneVerificationNotification.php
@@ -10,12 +10,13 @@ class PhoneVerificationNotification extends Notification
 {
     use Queueable;
 
-    private string $code;
+    private readonly string $code;
+    private readonly string $verificationToken;
 
-    public function __construct(string $code)
-    {
-        $this->code = $code;
-    }
+    public function __construct(
+        private readonly string $code,
+        private readonly string $verificationToken
+    ) {}
 
     public function via($notifiable): array
     {
@@ -24,8 +25,11 @@ class PhoneVerificationNotification extends Notification
 
     public function toTwilio($notifiable): array
     {
+        $verificationUrl = config('app.frontend_url') . '/verify-registration?token=' . $this->verificationToken;
+        
         return [
-            'content' => "Sağlık Pusulam doğrulama kodunuz: {$this->code}"
+            'content' => "Sağlık Pusulam doğrulama kodunuz: {$this->code}\n\n".
+                        "Doğrulama sayfası: {$verificationUrl}"
         ];
     }
 }
